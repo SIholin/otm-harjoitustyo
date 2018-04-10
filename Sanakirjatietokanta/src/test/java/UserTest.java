d7f143aa1af2dcf64bd233c3e53d004489cc6c49
SIholin <iholin.sanna@gmail.com>
2018-04-10 21:13:49 +0300

    testit kuntoon : ^ )

--- Sanakirjatietokanta/src/test/java/UserTest.java
+++ Sanakirjatietokanta/src/test/java/UserTest.java
@@ -16,7 +16,23 @@ public class UserTest {
     
     @Test
     public void kayttajaOlemassa(){
-        assertTrue(user!=null);
+        assertTrue(user != null);
+    }
+    
+    @Test
+    public void oikeaKayttaja(){
+        assertTrue(user.getUsername().equals("ihqsanna"));
+    }
+    
+    @Test
+    public void equalsToimii() {
+        User user2 = new User(2, "palleroinen");
+        User user3 = new User(3, "ihqsanna");
+        
+        assertTrue(user.equals(user3));
+        assertFalse(user.equals(user2));
+        assertTrue(user.equals(user));
+        
     }
     
     
