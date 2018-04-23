d365e9f09cd88194a3736f6a4910c6b6d9c13e64
SIholin <iholin.sanna@gmail.com>
2018-04-24 00:34:44 +0300

    testit word luokalle

--- Sanakirjatietokanta/src/test/java/Tests/UserTest.java
+++ Sanakirjatietokanta/src/test/java/Tests/UserTest.java
@@ -15,20 +15,26 @@ public class UserTest {
     }
 
     @Test
-    public void kayttajaOlemassa() {
+    public void userExsists() {
         assertTrue(user != null);
+    }
+    
+    @Test
+    public void idIsRight() {
+        assertTrue(user.getId() == 1);
     }
 
     @Test
-    public void oikeaKayttaja() {
+    public void rightUser() {
         assertTrue(user.getUsername().equals("ihqsanna"));
     }
 
     @Test
-    public void equalsToimii() {
+    public void equalswWorks() {
         User user2 = new User(2, "palleroinen");
         User user3 = new User(3, "ihqsanna");
-
+        
+        assertFalse(user.equals("pieru"));
         assertTrue(user.equals(user3));
         assertFalse(user.equals(user2));
         assertTrue(user.equals(user));
---
+++ Sanakirjatietokanta/src/test/java/Tests/WordTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tests;
+
+import Sanakirja.Domain.Word;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author ihqsanna
+ */
+public class WordTest {
+    
+    Word word;
+    
+    @Before
+    public void setUp() {
+        word = new Word(1, "kissa", "cat");
+    }
+    
+    @Test
+    public void wordExsists() {
+        assertTrue(word != null);
+    }
+    
+    @Test
+    public void idWorks() {
+        assertTrue(word.getId() == 1);
+    }
+    
+    @Test
+    public void formIsRight() {
+        assertTrue(word.getForm().equals("kissa"));
+    }
+    
+    @Test
+    public void translationIsRight() {
+        assertTrue(word.getTransaltion().equals("cat"));
+    }
+    
+}
